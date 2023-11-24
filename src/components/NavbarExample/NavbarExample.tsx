29c730d91a6e8607e4893a4f51a072563417f218
Lejiete <linda.lejiete@accenture.com>
2023-11-24 14:08:28 +0200

    added router, provider and links in navbar

--- src/components/NavbarExample/NavbarExample.tsx
+++ src/components/NavbarExample/NavbarExample.tsx
@@ -2,17 +2,23 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Logo from '../../assets/ACN.svg'
+import { Link } from 'react-router-dom'
 
 const NavbarExample = () => {
   return (
-    <Navbar className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img alt="" src={Logo} width="30" height="30" className="d-inline-block align-top" />{' '}
-          <p></p>
-        </Navbar.Brand>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <Link to="/">
+              <img alt="" src={Logo} width="30" height="30" className="d-inline-block align-top" />{' '}
+            </Link>
+            <Link to="/carousel">Carousel</Link>
+            <Link to="/accordion">Accordion</Link>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
